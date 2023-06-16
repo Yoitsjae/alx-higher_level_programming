1676cf7027d12ba4c18668b576ca70a735fb7c0c
Yoitajae <ncubejason896@gmail.com>
2023-06-16 05:07:18 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
-)
+#!/usr/bin/python3
+
+from typing import List
+
+def search_replace(my_list: List[int], search: int, replace: int) -> List[int]:
+    new_list = [replace if num == search else num for num in my_list]
+    return new_list
