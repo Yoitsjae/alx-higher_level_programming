6c25e9f50d9036c5097bf1a25ea39dcd17627e90
Yoitajae <ncubejason896@gmail.com>
2023-07-12 14:49:38 -0700

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as file:
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def write_file(filename="", text=""):
     with open(filename, 'w', encoding='utf-8') as file:
