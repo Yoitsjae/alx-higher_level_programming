f85de7934101dfb17f2f8991d531e21d134bef51
Yoitsjae <ncubejason896@gmail.com>
2023-09-11 15:07:33 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,15 +2,7 @@
 
 def lookup(obj):
     """Returns a list of attributes and methods of an object."""
-    result = []
-    for attr_name in dir(obj):
-        # Filter out attributes and methods inherited from 'object' class
-        if not attr_name.startswith('__'):
-            attr = getattr(obj, attr_name)
-            # Check if the attribute is a method or a callable function
-            if callable(attr):
-                result.append(attr_name)
-    return result
+    return [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
 
 if __name__ == "__main__":
     class MyClass1(object):
@@ -24,4 +16,4 @@ if __name__ == "__main__":
     print(lookup(MyClass1))  # Check 0
     print(lookup(MyClass2))  # Check 1
     print(lookup(int))       # Check 2
-    print(lookup([]))        # Check 3
+    print(lookup([]))        # Check 33
